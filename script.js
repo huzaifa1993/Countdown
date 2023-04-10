d1bc82c7648d11b4ad83f9ea76f2542d73b0d293
huzaifa1993 <huothyfayusuf1993@gmail.com>
2023-04-10 14:23:37 +0100

    firing the submit button

--- script.js
+++ script.js
@@ -2,6 +2,19 @@ const inputContainer = document.getElementById('input-container');
 const countDownForm = document.getElementById('countdownForm');
 const dateEl = document.getElementById('date-picker');
 
+let countdownTitle = '';
+let countdownDate = '';
+
+
 const today = new Date().toISOString().split('T')[0];
-// console.log(today)
 dateEl.setAttribute('min', today);
+
+
+function updateCountdown(e) {
+    e.preventDefault();
+    countdownTitle = e.srcElement[0].value;
+    countdownDate = e.srcElement[1].value;
+    console.log(countdownTitle, countdownDate);
+}
+
+countDownForm.addEventListener('submit', updateCountdown);
